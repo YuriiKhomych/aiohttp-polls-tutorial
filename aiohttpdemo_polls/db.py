dc7652bb43a0b8676d51dc40b067d49406c1d1c1
yuriikhomych <yuriykhomich@gmail.com>
2018-12-11 21:48:45 +0200

    Create connection engine and graceful shutdown

--- aiohttpdemo_polls/db.py
+++ aiohttpdemo_polls/db.py
@@ -1,3 +1,4 @@
+import aiopg.sa
 from sqlalchemy import (
     MetaData, Table, Column, ForeignKey,
     Integer, String, Date
@@ -24,3 +25,20 @@ choice = Table(
            Integer,
            ForeignKey('question.id', ondelete='CASCADE'))
 )
+
+async def init_pg(app):
+    conf = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(
+        database=conf['database'],
+        user=conf['user'],
+        password=conf['password'],
+        host=conf['host'],
+        port=conf['port'],
+        minsize=conf['minsize'],
+        maxsize=conf['maxsize'],
+    )
+    app['db'] = engine
+
+async def close_pg(app):
+    app['db'].close()
+    await app['db'].wait_closed()
--- aiohttpdemo_polls/main.py
+++ aiohttpdemo_polls/main.py
@@ -1,9 +1,12 @@
 from aiohttp import web
 
+from db import init_pg, close_pg
 from settings import config
 from routes import setup_routes
 
 app = web.Application()
 setup_routes(app)
 app['config'] = config
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
 web.run_app(app)
