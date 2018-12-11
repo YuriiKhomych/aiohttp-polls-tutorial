e7245895328cc110734b30ea1ee13b52bb8d0334
yuriikhomych <yuriykhomich@gmail.com>
2018-12-11 22:14:54 +0200

    Write Templates part

--- aiohttpdemo_polls/main.py
+++ aiohttpdemo_polls/main.py
@@ -1,4 +1,6 @@
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 from db import init_pg, close_pg
 from settings import config
@@ -7,6 +9,8 @@ from routes import setup_routes
 app = web.Application()
 setup_routes(app)
 app['config'] = config
+aiohttp_jinja2.setup(
+    app, loader=jinja2.PackageLoader('aiohttpdemo_polls', 'templates'))
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
 web.run_app(app)
--- aiohttpdemo_polls/views.py
+++ aiohttpdemo_polls/views.py
@@ -1,5 +1,22 @@
 from aiohttp import web
+import aiohttp_jinja2
 
+from . import db
 
 async def index(request):
     return web.Response(text='Hello Aiohttp!')
+
+
+@aiohttp_jinja2.template('detail.html')
+async def poll(request):
+    async with request.app['db'].acquire() as conn:
+        question_id = request.match_info['question_id']
+        try:
+            question, choices = await db.get_question(conn,
+                                                      question_id)
+        except db.RecordNotFound as e:
+            raise web.HTTPNotFound(text=str(e))
+        return {
+            'question': question,
+            'choices': choices
+        }
